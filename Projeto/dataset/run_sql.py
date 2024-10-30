68b95635f2f438e90bb00cb0fefdb80a90a9ac1f
AntonioFreire203 <freireantonio203@gmail.com>
2024-10-30 11:14:24 -0300

    update file concetor database

--- Projeto/dataset/run_sql.py
+++ Projeto/dataset/run_sql.py
@@ -1,24 +1,27 @@
-# Módulo de conexão ao banco de dados
-
-# pip install psycopg2
-# https://pypi.org/project/psycopg2/
-
-# Imports
 import os
 import psycopg2
 import psycopg2.extras as ext
+from dotenv import load_dotenv
 
-# Função
-def run_sql(sql, values = None):
+# Carregar as variáveis do arquivo .env
+load_dotenv()
 
+def run_sql(sql, values=None):
     # Variáveis de controle
     conn = None
     results = []
 
     # Conexão ao banco de dados
     try:
-        conn = psycopg2.connect("host=localhost port=5432 dbname=dbapp user=postgres password=admin")
-        cur = conn.cursor(cursor_factory = ext.DictCursor)
+        
+        conn = psycopg2.connect(
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT'),
+            dbname=os.getenv('DB_NAME'),
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD')
+        )
+        cur = conn.cursor(cursor_factory=ext.DictCursor)
         cur.execute(sql, values)
         conn.commit()
         results = cur.fetchall()
@@ -28,5 +31,5 @@ def run_sql(sql, values = None):
     finally:
         if conn is not None:
             conn.close()
-            
+
     return results
